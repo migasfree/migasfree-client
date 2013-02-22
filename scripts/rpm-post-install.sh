a691a5a8ffde2e0846df78c58a7b43e73599fb4d
Jose Antonio Chavarría <jachavar@gmail.com>
2013-02-22 19:53:39 +0100

    Deleted chmod of bin files for RPM packages

--- scripts/rpm-post-install.sh
+++ scripts/rpm-post-install.sh
@@ -3,8 +3,5 @@
 # This file becomes the post-install section of the generated spec file.
 #
 
-# ensure permissions of bin files
-chmod 700 /usr/bin/migasfree*
-
 #%update_mime_database
 gtk-update-icon-cache --quiet /usr/share/icons/hicolor/
