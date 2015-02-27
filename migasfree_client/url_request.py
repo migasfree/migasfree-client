1155702041a6f594a54029957a80de95ff3b6a7e
Jose Antonio Chavarría <jachavar@gmail.com>
2015-02-27 20:43:29 +0100

    Curl has been replaced by requests for humans (http://docs.python-requests.org/)

--- migasfree_client/url_request.py
+++ migasfree_client/url_request.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2014 Jose Antonio Chavarría
+# Copyright (c) 2011-2015 Jose Antonio Chavarría
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,23 +24,22 @@ import os
 import sys
 import errno
 import logging
-import pycurl
+import requests
+import json
 
 import secure
-import curl
 import utils
-import server_errors
 
 import gettext
 _ = gettext.gettext
 
-from settings import TMP_PATH
+from . import settings
 
 
 class UrlRequest(object):
     _debug = False
 
-    _sign = True
+    _safe = True
     _exit_on_error = True
 
     _proxy = ''
@@ -52,20 +51,21 @@ class UrlRequest(object):
         debug=False,
         proxy='',
         url_base='',
-        info_keys={},
+        project='',
+        keys={},
         cert=None
     ):
         self._debug = debug
         self._proxy = proxy
         self._url_base = url_base
+        self._project = project
         self._cert = cert
 
         logging.info('SSL certificate: %s', self._cert)
 
-        if type(info_keys) is dict:
-            self._path_keys = info_keys.get('path')
-            self._private_key = info_keys.get('private')
-            self._public_key = info_keys.get('public')
+        if type(keys) is dict:
+            self._private_key = keys.get('private')
+            self._public_key = keys.get('public')
 
         if self._proxy:
             logging.info('Proxy selected: %s', self._proxy)
@@ -75,160 +75,137 @@ class UrlRequest(object):
         else:
             logging.critical('URL base not assigned!!!')
 
-        if self._path_keys:
-            logging.info('Path keys: %s', self._path_keys)
-
         if self._private_key:
             logging.info('Private key: %s', self._private_key)
 
         if self._public_key:
             logging.info('Public key: %s', self._public_key)
 
-        self._filename_pattern = '%s.%s' % (
-            utils.get_mfc_computer_name(),
-            utils.get_hardware_uuid()
-        )
+    def _check_tmp_path(self):
+        if not os.path.exists(settings.TMP_PATH):
+            try:
+                os.makedirs(settings.TMP_PATH, 0777)
+            except:
+                return False
+
+        return True
 
     def run(
         self,
-        cmd,
+        end_point,
         data='',
         upload_file=None,
-        sign=True,
+        safe=True,
         exit_on_error=True
     ):
+        self._exit_on_error = exit_on_error
+
         logging.debug('URL base: %s', self._url_base)
-        logging.debug('URL command: %s', cmd)
+        logging.debug('URL end point: %s', end_point)
         logging.debug('URL data: %s', data)
         logging.debug('URL upload file: %s', upload_file)
-        logging.debug('Sign request: %s', sign)
+        logging.debug('Safe request: %s', safe)
         logging.debug('Exit on error: %s', exit_on_error)
 
-        if not os.path.exists(TMP_PATH):
-            try:
-                os.makedirs(TMP_PATH, 0777)
-            except:
-                _msg = 'Error creating %s directory' % TMP_PATH
+        headers = None
+        if safe:
+            data = json.dumps({
+                'msg': secure.wrap(
+                    data,
+                    sign_key=self._private_key,
+                    encrypt_key=self._public_key
+                ),
+                'project': self._project
+            })
+            headers = {'content-type': 'application/json'}
+
+        # FIXME multiple files
+        files = None
+        if upload_file:
+            files = {'file': utils.read_file(upload_file)}
+
+        proxies = None
+        if self._proxy:
+            proxies = {
+              "http": self._proxy,
+              "https": self._proxy,
+            }
+
+        url = self._url_base + end_point
+        if not url.endswith('/'):
+            url += '/'
+
+        r = requests.post(
+            url, data=data, headers=headers,
+            files=files, proxies=proxies, cert=self._cert
+        )
+
+        if r.status_code != requests.codes.ok:
+            self._error_response(r, end_point)
+
+        return self._evaluate_response(r.json(), safe)
+
+    def _evaluate_response(self, json, safe):
+        if safe:
+            response = secure.unwrap(
+                json['msg'],
+                decrypt_key=self._private_key,
+                verify_key=self._public_key
+            )
+        else:
+            response = json
+
+        logging.debug('Response text: %s' % response)
+        if self._debug:
+            print(response)
+
+        return response
+
+    def _error_response(self, request, end_point):
+        logging.error(
+            'url_request server error response code: %s',
+            str(request.status_code)
+        )
+        logging.error(
+            'url_request server error response info: %s',
+            str(request.text)
+        )
+
+        print(_('HTTP error code: %s') % request.status_code)
+        if self._debug:
+            if not self._check_tmp_path():
+                msg = _('Error creating %s directory') % settings.TMP_PATH
                 logging.exception(_msg)
+                print(msg)
+                if self._exit_on_error:
+                    sys.exit(errno.EPERM)
+
                 return {
-                    'errmfs': {
-                        'info': _msg,
-                        'code': server_errors.GENERIC
+                    'error': {
+                        'info': msg,
+                        'code': errno.EPERM
                     }
                 }
 
-        _filename = os.path.join(
-            TMP_PATH,
-            '%s.%s' % (
-                self._filename_pattern,
-                cmd
-            )
-        )
-        if self._debug:
-            print(_filename)
-        if sign:
-            secure.wrap(
-                _filename,
-                {cmd: data},
-                key=os.path.join(self._path_keys, self._private_key)
-            )
-        else:
-            secure.wrap(_filename, {cmd: data})
-
-        _post = [
-            ('message', (pycurl.FORM_FILE, _filename))
-        ]
-        if upload_file:
-            _post.append(('package', (pycurl.FORM_FILE, upload_file)))
-
-        logging.debug('Post data: %s', _post)
-
-        _curl = curl.Curl(
-            self._url_base,
-            _post,
-            proxy=self._proxy,
-            cert=self._cert,
-        )
-        _curl.run()
-        if not self._debug:
-            os.remove(_filename)
-
-        if _curl.error:
-            _msg = _('Curl error: %s') % _curl.error
-            logging.error(_msg)
-            print(_msg)
-
-            return {'errmfs': {'info': _msg, 'code': server_errors.GENERIC}}
-
-        if _curl.http_code >= 400:
-            print(_('HTTP error code: %s') % _curl.http_code)
-            if self._debug:
-                _file = os.path.join(
-                    TMP_PATH,
-                    'response.%s.%s.html' % (
-                        _curl.http_code,
-                        cmd
-                    )
+            extension = 'txt' if 'json' in request.headers['content-type'] else 'html'
+            _file = os.path.join(
+                settings.TMP_PATH,
+                'response.%s.%s.%s' % (
+                    request.status_code,
+                    end_point.replace('/', '.').rstrip('.'),
+                    extension
                 )
-                utils.write_file(_file, str(_curl.body))
-                print(_file)
-
-            return {
-                'errmfs': {
-                    'info': str(_curl.body),
-                    'code': server_errors.GENERIC
-                }
-            }
-
-        # evaluate response
-        _response = '%s.return' % _filename
-        utils.write_file(_response, str(_curl.body))
-        if sign:
-            _ret = secure.unwrap(
-                _response,
-                key=os.path.join(self._path_keys, self._public_key)
             )
-        else:
-            _ret = secure.unwrap(_response)
+            utils.write_file(_file, str(request.text))
+            print(_file)
 
-        if not self._debug:
-            os.remove(_response)
-        else:
-            print(_response)
-
-        if not type(_ret) is dict or not ('%s.return' % cmd) in _ret:
-            if 'errmfs' in _ret:
-                _msg = server_errors.error_info(_ret['errmfs']['code'])
-                logging.error(_msg)
-                print(_msg)
-
-            _msg = 'url_request unexpected response: %s. Expected: %s'
-            if self._debug:
-                print(_msg % (_ret, '%s.return' % cmd))
-
-            logging.critical(_msg, _ret, '%s.return' % cmd)
+        if exit_on_error:
+            print(_('Error: %s') % str(request.text))
             sys.exit(errno.EACCES)
 
-        _ret = _ret['%s.return' % cmd]  # unwrapping cmd response
-        if type(_ret) is dict and 'errmfs' in _ret:
-            if _ret['errmfs']['code'] != server_errors.ALL_OK:
-                _error = server_errors.error_info(_ret['errmfs']['code'])
-                if self._debug:
-                    print(_('Error: %s') % _error)
-                    if _ret['errmfs']['info']:
-                        print(_('Information: %s') % _ret['errmfs']['info'])
-
-                logging.error(
-                    'url_request server error response code: %s',
-                    _error
-                )
-                logging.error(
-                    'url_request server error response info: %s',
-                    _ret['errmfs']['info']
-                )
-
-                if exit_on_error:
-                    print(_('Error: %s') % _error)
-                    sys.exit(errno.EACCES)
-
-        return _ret
+        return {
+            'error': {
+                'info': str(request.text),
+                'code': request.status_code
+            }
+        }
