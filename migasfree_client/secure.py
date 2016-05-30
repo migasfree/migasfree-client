19ab6c198e22ddcb5b0fdacd9e96e3e27be2ebf8
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 14:06:53 +0200

    Removed unused import

--- migasfree_client/secure.py
+++ migasfree_client/secure.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2011-2015 Jose Antonio Chavarría
+# Copyright (c) 2011-2016 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,13 +14,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Author: Jose Antonio Chavarría <jachavar@gmail.com>
 
 __author__ = "Jose Antonio Chavarría"
 __license__ = 'GPLv3'
-
-import os
 
 from Crypto.PublicKey import RSA
 
