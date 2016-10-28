aedc9fd12ac5017249a740d01408257ef14be058
Jose Antonio Chavarría <jachavar@gmail.com>
2016-10-28 14:40:20 +0200

    Updated server errors (issue migasfree/migasfree#193)

--- migasfree_client/server_errors.py
+++ migasfree_client/server_errors.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2013 Jose Antonio Chavarría
+# Copyright (c) 2011-2016 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,40 +14,41 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Author: Jose Antonio Chavarría <jachavar@gmail.com>
 
 __author__ = 'Jose Antonio Chavarría'
-__file__ = 'server_errors.py'
-__date__ = '2013-01-26'
+__license__ = 'GPLv3'
 
 # TODO common code between server & client
 
 import gettext
 _ = gettext.gettext
 
-ALL_OK               = 0
-NO_AUTHENTICATED     = 1
-CAN_NOT_REGISTER     = 2
-METHOD_GET_NOT_ALLOW = 3
-COMMAND_NOT_FOUND    = 4
-SIGN_NOT_OK          = 5
-COMPUTER_NOT_FOUND   = 6
-DEVICE_NOT_FOUND     = 7
-VERSION_NOT_FOUND    = 8
-GENERIC              = 100
+ALL_OK = 0
+UNAUTHENTICATED = 1
+CAN_NOT_REGISTER_COMPUTER = 2
+GET_METHOD_NOT_ALLOWED = 3
+COMMAND_NOT_FOUND = 4
+INVALID_SIGNATURE = 5
+COMPUTER_NOT_FOUND = 6
+DEVICE_NOT_FOUND = 7
+VERSION_NOT_FOUND = 8
+USER_DOES_NOT_HAVE_PERMISSION = 9
+UNSUBSCRIBED_COMPUTER = 10
+GENERIC = 100
 
 ERROR_INFO = {
-    ALL_OK              : _("No errors"),
-    NO_AUTHENTICATED    : _("User not authenticated"),
-    CAN_NOT_REGISTER    : _("User can not register computers"),
-    METHOD_GET_NOT_ALLOW: _("Method GET not allowed"),
-    COMMAND_NOT_FOUND   : _("Command not found"),
-    SIGN_NOT_OK         : _("Signature is not valid"),
-    COMPUTER_NOT_FOUND  : _("Computer not found"),
-    DEVICE_NOT_FOUND    : _("Device not found"),
-    VERSION_NOT_FOUND   : _("Version not found"),
-    GENERIC             : _("Generic error")
+    ALL_OK: _("No errors"),
+    UNAUTHENTICATED: _("User unauthenticated"),
+    CAN_NOT_REGISTER_COMPUTER: _("User can not register computers"),
+    GET_METHOD_NOT_ALLOWED: _("Method GET not allowed"),
+    COMMAND_NOT_FOUND: _("Command not found"),
+    INVALID_SIGNATURE: _("Signature is not valid"),
+    COMPUTER_NOT_FOUND: _("Computer not found"),
+    DEVICE_NOT_FOUND: _("Device not found"),
+    VERSION_NOT_FOUND: _("Version not found"),
+    USER_DOES_NOT_HAVE_PERMISSION: _("User does not have permission"),
+    UNSUBSCRIBED_COMPUTER: _("Unsubscribed computer"),
+    GENERIC: _("Generic error")
 }
 
 
@@ -55,7 +56,4 @@ def error_info(number):
     '''
     string error_info(int number)
     '''
-    if number in ERROR_INFO:
-        return ERROR_INFO[number]
-
-    return ''  # if not found
+    return ERROR_INFO.get(number, '')
