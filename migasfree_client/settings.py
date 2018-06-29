1ebe0f5a93b45136ea64347d7c0dfeed59fe23af
Jose Antonio Chavarría <jachavar@gmail.com>
2018-06-29 13:41:16 +0200

    Added APP_DATA_PATH, PRE_SYNC_PATH and POST_SYNC_PATH constants (issue #84)

--- migasfree_client/settings.py
+++ migasfree_client/settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2018 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,8 +27,11 @@ LOG_FILE = '/var/tmp/migasfree.log'
 SOFTWARE_FILE = '/var/tmp/installed_software.txt'
 
 KEYS_PATH = '/var/migasfree-client/keys'
-DEVICES_PATH = "/var/migasfree-client/devices/"
+DEVICES_PATH = '/var/migasfree-client/devices'
 TMP_PATH = '/tmp/migasfree-client'
 LOCALE_PATH = '/usr/share/locale'
 ICON_PATH = '/usr/share/icons/hicolor/scalable'
 DOC_PATH = '/usr/share/doc/migasfree-client'
+APP_DATA_PATH = '/usr/share/migasfree-client'
+PRE_SYNC_PATH = os.path.join(APP_DATA_PATH, 'pre-sync.d')
+POST_SYNC_PATH = os.path.join(APP_DATA_PATH, 'post-sync.d')
