c0b9452cad30cd6fa4aadacb3e7c21a250092fb6
Jose Antonio Chavarría <jachavar@gmail.com>
2019-04-08 13:28:36 +0200

    Changed shebang to python2 (#97)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2018 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2019 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
