84a51634759876e098ffe6b9ac57fe88a8af5f5b
Jose Antonio Chavarría <jachavar@gmail.com>
2019-07-19 11:24:27 +0200

    Verbose output in _execute_path method

--- migasfree_client/command.py
+++ migasfree_client/command.py
@@ -229,6 +229,7 @@ class MigasFreeCommand(object):
             self._send_message(_('Running command %s...') % file_)
             _ret, _output, _error = utils.execute(
                 os.path.join(path, file_),
+                verbose=True,
                 interactive=False
             )
             if _ret == 0:
