72566401aa44624a3f030e5f92d826d460eca199
Jose Antonio Chavarría <jachavar@gmail.com>
2019-07-24 08:37:14 +0200

    Prepare code to Python 3

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class BuildData(build):
                     rc = subprocess.call(['msgfmt', '-o', mo, po])
                     if rc != 0:
                         raise Warning("msgfmt returned %d" % rc)
-                except Exception, e:
+                except Exception as e:
                     error("Building gettext files failed.  Try setup.py \
                         --without-gettext [build|install]")
                     error("Error: %s" % str(e))
