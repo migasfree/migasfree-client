212b32a47ee394d51b0b0f5567144c5ffc118cd3
Jose Antonio Chavarría <jachavar@gmail.com>
2019-07-24 08:36:56 +0200

    Better error detection in _sync_logical_devices method (python-cups)

--- migasfree_client/client.py
+++ migasfree_client/client.py
@@ -791,9 +791,17 @@ class MigasFreeClient(MigasFreeCommand):
 
         try:
             conn = cups.Connection()
-        except (RuntimeError, NameError):
+        except RuntimeError:
             self._show_message(_('Synchronizing logical devices...'))
             _msg = _('CUPS is not running!!!')
+            self.operation_failed(_msg)
+            logging.error(_msg)
+            self._write_error(_msg)
+
+            return
+        except NameError:
+            self._show_message(_('Synchronizing logical devices...'))
+            _msg = _('Python CUPS is required. If not, configure Manage_Devices parameter to False.')
             self.operation_failed(_msg)
             logging.error(_msg)
             self._write_error(_msg)
