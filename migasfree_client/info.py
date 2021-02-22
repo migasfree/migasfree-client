0534459829eace9faab7162b0a9dd4cae930e2ac
jact <jachavar@gmail.com>
2021-02-22 13:57:24 +0100

    Use of python rich

--- migasfree_client/info.py
+++ migasfree_client/info.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2018-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2018-2021 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,6 +29,8 @@ _ = gettext.gettext
 import logging
 logger = logging.getLogger(__name__)
 
+from rich.table import Table
+
 from .command import MigasFreeCommand
 
 
@@ -42,17 +44,18 @@ class MigasFreeInfo(MigasFreeCommand):
             self.get_computer_id()
 
         logger.debug('Getting label')
-        response = self._url_request.run(
-            url=self.api_endpoint(self.URLS['get_label']),
-            data={
-                'id': self._computer_id
-            },
-            debug=self._debug
-        )
+        with self.console.status(''):
+            response = self._url_request.run(
+                url=self.api_endpoint(self.URLS['get_label']),
+                data={
+                    'id': self._computer_id
+                },
+                debug=self._debug
+            )
 
         logger.debug('Response get_label: {}'.format(response))
         if self._debug:
-            print('Response: {}'.format(response))
+            self.console.log('Response: {}'.format(response))
 
         if 'error' in response:
             self.operation_failed(response['error']['info'])
@@ -65,26 +68,44 @@ class MigasFreeInfo(MigasFreeCommand):
             sys.exit(errno.EPERM)
 
         info = self.get_label()
+        table = Table(show_header=True, header_style='bold')
 
         if not key:
             if not self._quiet:
-                print('ID\tNAME\tSEARCH\tUUID')
-            print(
-                '{}\t{}\t{}\t{}'.format(
-                    self._computer_id,
+                table.add_column('ID')
+                table.add_column('NAME')
+                table.add_column('SEARCH')
+                table.add_column('UUID')
+                table.add_row(
+                    str(self._computer_id),
                     info['name'],
                     info['search'],
                     info['uuid']
                 )
-            )
+                self.console.print(table)
+            else:
+                print(
+                    '{}\t{}\t{}\t{}'.format(
+                        self._computer_id,
+                        info['name'],
+                        info['search'],
+                        info['uuid']
+                    )
+                )
         elif key == 'id':
             if not self._quiet:
-                print('ID')
-            print(self._computer_id)
+                table.add_column('ID')
+                table.add_row(str(self._computer_id))
+                self.console.print(table)
+            else:
+                print(self._computer_id)
         else:
             if not self._quiet:
-                print(key.upper())
-            print(info[key])
+                table.add_column(key.upper())
+                table.add_row(info[key])
+                self.console.print(table)
+            else:
+                print(info[key])
 
     def run(self, args=None):
         if hasattr(args, 'debug') and args.debug:
