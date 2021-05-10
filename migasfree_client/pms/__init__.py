5b9b801366292ef004b66d43f65b8dce3e190897
jact <jachavar@gmail.com>
2021-05-10 13:10:56 +0200

    Added get_available_pms function

--- migasfree_client/pms/__init__.py
+++ migasfree_client/pms/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2019 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2021 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+import inspect
+import importlib
+import pkgutil
+
 from .pms import Pms
 from .apt import Apt
 from .yum import Yum
 from .zypper import Zypper
+from . import plugins
+
+
+def iter_namespace(ns_pkg):
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + '.')
+
+
+def get_discovered_plugins():
+    return {
+        name: importlib.import_module(name)
+        for finder, name, ispkg
+        in iter_namespace(plugins)
+    }
+
+
+def get_available_pms():
+    ret = [
+        ('apt', 'Apt'),
+        ('yum', 'Yum'),
+        ('zypper', 'Zypper'),
+    ]
+
+    discovered_plugins = get_discovered_plugins()
+
+    for item in discovered_plugins.keys():
+        for class_ in inspect.getmembers(sys.modules[item], inspect.isclass):
+            if class_[0] != 'Pms':
+                ret.append((class_[1]()._name, class_[0]))
+
+    return sorted(ret, key=lambda x: x[0])
