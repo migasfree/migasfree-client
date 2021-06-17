06c088a1f1575ea75aea2382f2d7563d1d065c73
jact <jachavar@gmail.com>
2021-06-17 09:52:37 +0200

    Added command to build in Windows platform

---
+++ bin/create-package.bat
@@ -0,0 +1,18 @@
+@echo off
+cd ..
+
+python setup.py bdist_wininst
+python setup.py bdist
+python setup.py bdist --format=msi
+
+echo "Python requirements:"
+echo "  * Python >= 3.6"
+echo "  * python-magic-bin"
+echo "  * psam-win"
+echo "  * pywin32"
+echo "  * psutil"
+echo "  * netifaces"
+echo "  * jwcrypto"
+echo "  * requests"
+echo "  * requests-toolbelt"
+echo "  * rich"
