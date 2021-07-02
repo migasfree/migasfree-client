1619f595af504bc0a17081a8a757c9de04ac8e8c
jact <jachavar@gmail.com>
2021-07-02 13:54:42 +0200

    Fixed pysam-win requirement

--- bin/create-package.bat
+++ bin/create-package.bat
@@ -8,7 +8,7 @@ python setup.py bdist --format=msi
 echo "Python requirements:"
 echo "  * Python >= 3.6"
 echo "  * python-magic-bin"
-echo "  * psam-win"
+echo "  * pysam-win"
 echo "  * pywin32"
 echo "  * psutil"
 echo "  * netifaces"
