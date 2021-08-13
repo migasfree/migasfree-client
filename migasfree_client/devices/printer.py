8e50ea4faa3e0ebb5470d46a01f96489ffd2d6ad
jact <jachavar@gmail.com>
2021-08-13 12:22:50 +0200

    Removed unused import

--- migasfree_client/devices/printer.py
+++ migasfree_client/devices/printer.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 __author__ = 'Jose Antonio Chavarría'
 __license__ = 'GPLv3'
 
