114838a5aae1f6ecd79f465dd2e783f60f33bfd1
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-23 08:20:13 +0100

    A class is inherited from object by default, so explicitly inheriting from object is redundant.

--- migasfree_client/devices/printer.py
+++ migasfree_client/devices/printer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2014-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2014-2022 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ __author__ = 'Jose Antonio Chavarría'
 __license__ = 'GPLv3'
 
 
-class Printer(object):
+class Printer:
     """
     Interface class
     Abstract methods
