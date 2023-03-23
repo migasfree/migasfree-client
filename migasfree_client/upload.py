9871e22bc442859181e8769b2bc1b5668bbea8b8
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-23 12:11:58 +0100

    docs: updated copyright date

--- migasfree_client/upload.py
+++ migasfree_client/upload.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2023 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
