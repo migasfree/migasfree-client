3f2c3b7be9d4a16dc0ed34159b5fc731d81964e4
jact <jachavar@gmail.com>
2024-05-17 12:14:40 +0200

    fix: use app logger instead of __name__ to unify log messages

--- migasfree_client/sync.py
+++ migasfree_client/sync.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2023 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2024 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ __license__ = 'GPLv3'
 __all__ = 'MigasFreeSync'
 
 _ = gettext.gettext
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('migasfree_client')
 
 
 class MigasFreeSync(MigasFreeCommand):
