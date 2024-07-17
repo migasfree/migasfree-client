35bb6533a8a2b2218146ac1af683a6c06b95eba9
Jose Antonio Chavarría <jachavar@gmail.com>
2024-07-17 08:55:51 +0200

    fix: locale.getdefaultlocale deprecation

--- migasfree_client/__init__.py
+++ migasfree_client/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2011-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2024 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 import locale
 import gettext
 import builtins
@@ -31,21 +32,27 @@ __contact__ = "fun.with@migasfree.org"
 __homepage__ = "https://github.com/migasfree/migasfree-client/"
 
 # i18n
-domain = 'migasfree-client'
-gettext.install(domain, LOCALE_PATH)
+DOMAIN = 'migasfree-client'
+gettext.install(DOMAIN, LOCALE_PATH)
 
 if not os.environ.get('LANG'):
-    os.environ['LANG'] = locale.getdefaultlocale()[0]
+    if sys.platform == 'win32':
+        import ctypes
 
-gettext.bindtextdomain(domain, LOCALE_PATH)
+        windll = ctypes.windll.kernel32
+        os.environ['LANG'] = locale.windows_locale[windll.GetUserDefaultUILanguage()]
+    else:
+        os.environ['LANG'] = locale.getlocale()[0]
+
+gettext.bindtextdomain(DOMAIN, LOCALE_PATH)
 if hasattr(gettext, 'bind_textdomain_codeset'):
-    gettext.bind_textdomain_codeset(domain, 'UTF-8')
-gettext.textdomain(domain)
+    gettext.bind_textdomain_codeset(DOMAIN, 'UTF-8')
+gettext.textdomain(DOMAIN)
 
 try:
-    locale.bindtextdomain(domain, LOCALE_PATH)
+    locale.bindtextdomain(DOMAIN, LOCALE_PATH)
     if hasattr(locale, 'bind_textdomain_codeset'):
-        locale.bind_textdomain_codeset(domain, 'UTF-8')
-    locale.textdomain(domain)
+        locale.bind_textdomain_codeset(DOMAIN, 'UTF-8')
+    locale.textdomain(DOMAIN)
 except AttributeError:
     pass
