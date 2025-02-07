b9d77c30c8a872103dcb246a8b196eeca574f811
jact <jachavar@gmail.com>
2025-02-07 12:48:01 +0100

    refactor: _check_user_is_root usage

--- migasfree_client/info.py
+++ migasfree_client/info.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2018-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2018-2025 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ logger = logging.getLogger('migasfree_client')
 
 class MigasFreeInfo(MigasFreeCommand):
     def __init__(self):
-        self._user_is_not_root()
+        self._check_user_is_root()
         super().__init__()
 
     def get_label(self):
