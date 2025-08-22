394cc051a53dc94283a1662cd8e3995b30cd892e
jact <jachavar@gmail.com>
2025-08-22 10:35:35 +0200

    fix: _silent_options (--force-yes) feat: added deb822 support in create_repos method

--- migasfree_client/pms/apt.py
+++ migasfree_client/pms/apt.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2011-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2011-2025 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,8 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import re
 import logging
+import tempfile
 
 from .pms import Pms
 from ..utils import execute, write_file
@@ -37,10 +39,12 @@ class Apt(Pms):
     def __init__(self):
         super().__init__()
 
-        self._name = 'apt'          # Package Management System name
+        self._name = 'apt'  # Package Management System name
         self._pm = '/usr/bin/dpkg'  # Package Manager command
         self._pms = 'DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get'  # Package Management System command
-        self._repo = '/etc/apt/sources.list.d/migasfree.list'  # Repositories file
+        self._repo_dir = '/etc/apt/sources.list.d'  # Repositories path
+        self._keyring_dir = '/etc/apt/trusted.gpg.d'
+
         self._mimetype = [
             'application/x-debian-package',
             'application/vnd.debian.binary-package',
@@ -53,7 +57,8 @@ class Apt(Pms):
                                '-o Dpkg::Options::=--force-confdef ' \
                                '-o Dpkg::Options::=--force-confold ' \
                                '-o Debug::pkgProblemResolver=1 ' \
-                               '--assume-yes --force-yes ' \
+                               '--assume-yes --allow-downgrades ' \
+                               '--allow-change-held-packages ' \
                                '--allow-unauthenticated --auto-remove'
 
     def install(self, package):
@@ -94,9 +99,9 @@ class Apt(Pms):
         cmd = f'{self._pms} {self._silent_options} dist-upgrade'
         logger.debug(cmd)
 
-        _ret, _, _error = execute(cmd, interactive=False, verbose=True)
+        ret, _, error = execute(cmd, interactive=False, verbose=True)
 
-        return _ret == 0, _error
+        return ret == 0, error
 
     def install_silent(self, package_set):
         """
@@ -113,9 +118,9 @@ class Apt(Pms):
         cmd = f'{self._pms} {self._silent_options} install {" ".join(package_set)}'
         logger.debug(cmd)
 
-        _ret, _, _error = execute(cmd, interactive=False, verbose=True)
+        ret, _, error = execute(cmd, interactive=False, verbose=True)
 
-        return _ret == 0, _error
+        return ret == 0, error
 
     def remove_silent(self, package_set):
         """
@@ -132,9 +137,9 @@ class Apt(Pms):
         cmd = f'{self._pms} {self._silent_options} purge {" ".join(package_set)}'
         logger.debug(cmd)
 
-        _ret, _, _error = execute(cmd, interactive=False, verbose=True)
+        ret, _, error = execute(cmd, interactive=False, verbose=True)
 
-        return _ret == 0, _error
+        return ret == 0, error
 
     def is_installed(self, package):
         """
@@ -183,6 +188,83 @@ class Apt(Pms):
 
         return result
 
+    def _adapt_sources(self, sources_content):
+        """
+        Adds 'Trusted: yes' in each block of sources content if not exists (deb822)
+        Deletes empty 'Signed-By' lines
+        """
+
+        trusted_line = 'Trusted: yes'
+
+        blocks = sources_content.split('\n\n')  # each block separated by empty line
+        new_blocks = []
+
+        for block in blocks:
+            lines = block.splitlines()
+            signed_by_index = None
+            for i, line in enumerate(lines):
+                line_lower = line.lower()
+                if line_lower.startswith('signed-by:'):
+                    value = line[10:].strip()
+                    if not value:
+                        signed_by_index = i
+
+            # If trusted not exists, add to the end
+            if all(not line.lower().startswith('trusted:') for line in lines):
+                lines.append(trusted_line)
+
+            # Delete empty Signed-By line if exists
+            if signed_by_index is not None:
+                lines.pop(signed_by_index)
+
+            new_blocks.append('\n'.join(lines))
+
+        return '\n\n'.join(new_blocks)
+
+    def _convert_list_to_sources(self, list_content):
+        """
+        Converts formated content .list to .sources format using 'apt modernize-sources'
+
+        Returns .sources content as string, or None if it fails
+        """
+
+        # Create temp file .list at _repo_dir
+        fd, list_path = tempfile.mkstemp(prefix='tmp_repo_', suffix='.list', dir=self._repo_dir)
+        os.close(fd)
+
+        try:
+            if not write_file(list_path, list_content):
+                logging.error('Error writing temp file %s', list_path)
+                return None
+
+            cmd = 'yes | {0} modernize-sources {1}'.format(self._pms, list_path)
+            ret, _, err = execute(cmd, interactive=False)
+            if ret != 0:
+                logging.error('apt modernize-sources failed: %s', str(err))
+                return None
+
+            sources_path = list_path[:-5] + ".sources"
+            if not os.path.isfile(sources_path):
+                logging.error('Generated .sources file not found: %s', sources_path)
+                return None
+
+            with open(sources_path) as f:
+                sources_content = f.read()
+
+            return self._adapt_sources(sources_content)
+
+        finally:  # Cleaning temp files
+            if os.path.isfile(list_path):
+                os.remove(list_path)
+
+            sources_path = list_path[:-5] + '.sources'
+            if os.path.isfile(sources_path):
+                os.remove(sources_path)
+
+            backup_path = list_path + '.bak'
+            if os.path.isfile(backup_path):
+                os.remove(backup_path)
+
     def create_repos(self, protocol, server, repositories):
         """
         bool create_repos(string protocol, string server, list repositories)
@@ -192,6 +274,26 @@ class Apt(Pms):
             f"{repo.get('source_template').format(protocol=protocol, server=server)}" for repo in repositories
         )
 
+        # Detect APT version (if fails, default to 2.x for compatibility)
+        cmd = f"{self._pms} --version | head -n1 | awk '{{print $2}}'"
+        logger.debug(cmd)
+
+        ret, output, _ = execute(cmd, interactive=False)
+        apt_version = output.strip() if ret == 0 else '2.0'
+        logging.debug('Detected APT version: %s', apt_version)
+
+        # Choose format by APT version
+        self._repo = os.path.join(self._repo_dir, 'migasfree.list')
+        try:
+            major_version = int(apt_version.split('.')[0])
+            if major_version >= 3:
+                content = self._convert_list_to_sources(content)
+                self._repo = os.path.join(self._repo_dir, 'migasfree.sources')
+        except Exception:
+            pass
+
+        logging.debug('Creating repos: %s', self._repo)
+
         return write_file(self._repo, content)
 
     def import_server_key(self, file_key):
@@ -200,10 +302,11 @@ class Apt(Pms):
         """
 
         name = file_key.rsplit('.', 1)[0].replace(KEYS_PATH, '').split('/')[1]
-        cmd = f'gpg --output /etc/apt/trusted.gpg.d/{name}.gpg --dearmor --yes {file_key} > /dev/null'
+        key_target = os.path.join(self._keyring_dir, f'{name}.gpg')
+        cmd = f'gpg --output {key_target} --dearmor --yes {file_key} > /dev/null'
         logger.debug(cmd)
 
-        return execute(cmd)[0] == 0
+        return execute(cmd, interactive=False)[0] == 0
 
     def get_system_architecture(self):
         """
@@ -213,9 +316,9 @@ class Apt(Pms):
         cmd = f'echo "$({self._pm} --print-architecture) $({self._pm} --print-foreign-architectures)"'
         logger.debug(cmd)
 
-        _ret, _arch, _ = execute(cmd, interactive=False)
+        ret, arch, _ = execute(cmd, interactive=False)
 
-        return _arch.strip() if _ret == 0 else ''
+        return arch.strip() if ret == 0 else ''
 
     def available_packages(self):
         """
@@ -225,6 +328,6 @@ class Apt(Pms):
         cmd = f'{self._pms_search} pkgnames'
         logger.debug(cmd)
 
-        _ret, _output, _error = execute(cmd, interactive=False)
+        ret, output, _ = execute(cmd, interactive=False)
 
-        return sorted(_output.strip().splitlines()) if _ret == 0 else []
+        return sorted(output.strip().splitlines()) if ret == 0 else []
