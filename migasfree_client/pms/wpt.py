1e343edffd415122112d470be8aadedb05a4b373
Jose Antonio Chavarría <jachavar@gmail.com>
2025-10-07 09:12:47 +0200

    refactor: create_repos method

--- migasfree_client/pms/wpt.py
+++ migasfree_client/pms/wpt.py
@@ -170,6 +170,8 @@ class Wpt(Pms):
         content = ''.join(
             f'{repo.get("source_template").format(protocol=protocol, server=server)}' for repo in repositories
         )
+        if not content:
+            return True
 
         return write_file(self._repo, content)
 
