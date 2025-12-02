f1f8d1e98c906f1967dcb22f86d4c70c266cd694
Jose Antonio Chavarría <jachavar@gmail.com>
2025-12-02 09:32:27 +0100

    refactor: ruff guidelines

--- migasfree_client/pms/dnf.py
+++ migasfree_client/pms/dnf.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2023-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2023-2025 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
