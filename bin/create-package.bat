10870260d99d7e71df5965ee205c7f9c5cf24708
jact <jachavar@gmail.com>
2025-12-15 16:33:12 +0100

    feat: added info to script

--- bin/create-package.bat
+++ bin/create-package.bat
@@ -1,18 +1,25 @@
 @echo off
+REM Copyright (c) 2011-2025 Jose Antonio Chavarría <jachavar@gmail.com>
+REM Script to create Windows distribution packages
+
 cd ..
 
+echo Creating Windows packages...
 python setup.py bdist_wininst
 python setup.py bdist
 python setup.py bdist --format=msi
 
-echo "Python requirements:"
-echo "  * Python >= 3.6"
-echo "  * python-magic-bin"
-echo "  * pysam-win"
-echo "  * pywin32"
-echo "  * psutil"
-echo "  * netifaces"
-echo "  * jwcrypto"
-echo "  * requests"
-echo "  * requests-toolbelt"
-echo "  * rich"
+echo.
+echo Package creation completed.
+echo.
+echo Python requirements:
+echo   * Python ^>= 3.6
+echo   * python-magic-bin
+echo   * pywin32
+echo   * psutil
+echo   * netifaces
+echo   * jwcrypto
+echo   * requests
+echo   * requests-toolbelt
+echo   * rich
+echo   * cryptography
