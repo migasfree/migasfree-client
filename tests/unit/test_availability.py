43192df8ed651e14b8c75fa0b25514e948f9602c
jact <jachavar@gmail.com>
2026-01-23 07:42:10 +0100

    fix: copyright date

--- tests/unit/test_availability.py
+++ tests/unit/test_availability.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2026 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2026 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
